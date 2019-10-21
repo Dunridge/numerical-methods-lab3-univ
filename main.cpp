dfe73f267c7d85b79fc1349e5eda81e7bd539216
Maksym Polinka <Polinka7max@gmail.com>
2019-10-21 20:41:36 +0300

    added and configured the eigenvalue method

--- main.cpp
+++ main.cpp
@@ -12,19 +12,29 @@ of scalar products)
 за допомогою do while циклу
 */
 
-// TODO: define a matrix here
+/*
+A = np.array([[8., 1., -4.],
+              [2., -6., 1.],
+              [-1., 1., 4.]])
+ * */
 
-void Input(int n,int A[][100])
-{
-    int i,j;
-    for (i=0;i<n;i++)
-        for (j=0;j<n;j++)
-            scanf("%d",&A[i][j]);
-}
 
-void Matr(int A[][100],int n)
+int A[3][3] = {{8, 1, -4},
+               {2, -6, 1},
+               {-1, 1, 4}};
+
+//you don't need this method
+//void Input(int n,int A[][100])
+//{
+//    int i,j;
+//    for (i=0;i<n;i++)
+//        for (j=0;j<n;j++)
+//            scanf("%d",&A[i][j]);
+//}
+
+void Matr(int A[][3],int n)
 {
-    float w0[100],w[100],summ=0,w0norm[100],e,d,d0;
+    float w0[3],w[3],summ=0,w0norm[3],e,d,d0;
     int i,j,k;
     for (i=0;i<n;i++)
         w0[i]=0;
@@ -52,15 +62,38 @@ void Matr(int A[][100],int n)
         summ=0;
     }
     while(e>0.001);
-    printf("%f\n",d);
-    for (i=0;i<n;i++)
-        printf("%f\n",w0norm[i]);
+    printf("%f\n",d); //determinant???
+    //finding the minimum values
+    double max = w0norm[0];
+    for(int it1 = 1; it1 < 4; it1++) {
+        if(w0norm[i] > max) {
+            max = w0norm[it1];
+        }
+    }
+
+    std::cout << "the maximum eigenvalue is: " << max << std::endl;
+
+    //finding the minimum values
+    double min = w0norm[0];
+
+    for(int it2 = 1; it2 < 3; it2++) {
+        if(w0norm[it2] < max) {
+            min = w0norm[it2];
+        }
+    }
+
+    std::cout << "the minimum eigenvalue is: " << min << std::endl;
+
+    //print all of the numbers:
+    //for (i=0;i<n;i++)
+    //    printf("%f\n",w0norm[i]);
 }
 
 void main()
 {
-    int n,A[100][100];
-    scanf("%d",&n);
-    Input(n,A);
+    //int n,A[100][100];
+    int n = 3;
+    //scanf("%d",&n);
+    //Input(n,A);
     Matr(A,n);
 }
